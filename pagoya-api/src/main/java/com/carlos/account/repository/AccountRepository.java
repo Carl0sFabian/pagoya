db8cf2d87a0a5bb5fee768a50c614337b194a2d5
Carl0sFabian <fabian.mendoza.quispe@gmail.com>
2026-05-16 22:42:10 -0500

    feat(reports): analytical reports via PostgreSQL functions

--- pagoya-api/src/main/java/com/carlos/account/repository/AccountRepository.java
+++ pagoya-api/src/main/java/com/carlos/account/repository/AccountRepository.java
@@ -9,10 +9,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
     boolean existsByCustomerIdAndType(Long customerId, AccountType type);
+
     Optional<Account> findByAccountNumber(String accountNumber);
 
     @Query(value = "SELECT * FROM fn_account_summary_report()", nativeQuery = true)
     List<Object[]> reportSummaryRaw();
+
+    Page<Account> findByCustomerId(Long customerId, Pageable pageable);
 }
